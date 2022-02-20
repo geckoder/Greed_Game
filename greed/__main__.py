4517f9d791509f3bbf9bc50cb1e556c1a79216d7
Emily <cas20037@byui.edu>
2022-02-19 18:44:22 -0700

    Add gems and rock characters

--- greed/__main__.py
+++ greed/__main__.py
@@ -13,7 +13,6 @@ from game.services.video_service import VideoService
 from game.shared.color import Color
 from game.shared.point import Point
 
-
 FRAME_RATE = 12
 MAX_X = 900
 MAX_Y = 600
@@ -28,10 +27,10 @@ DEFAULT_ARTIFACTS = 40
 
 
 def main():
-    
+
     # create the cast
     cast = Cast()
-    
+
     # create the banner
     banner = Actor()
     banner.set_text("")
@@ -39,7 +38,7 @@ def main():
     banner.set_color(WHITE)
     banner.set_position(Point(CELL_SIZE, 0))
     cast.add_actor("banners", banner)
-    
+
     # create the robot
     x = int(MAX_X / 2)
     y = int(MAX_Y / 2)
@@ -51,14 +50,15 @@ def main():
     robot.set_color(WHITE)
     robot.set_position(position)
     cast.add_actor("robots", robot)
-    
+
     # create the artifacts
     with open(DATA_PATH) as file:
         data = file.read()
         messages = data.splitlines()
 
     for n in range(DEFAULT_ARTIFACTS):
-        text = chr(random.randint(33, 126))
+        # chooses between two characters (42 = *, 79 = O)
+        text = chr(random.choice([42, 79]))
         message = messages[n]
 
         x = random.randint(1, COLS - 1)
@@ -70,15 +70,15 @@ def main():
         g = random.randint(0, 255)
         b = random.randint(0, 255)
         color = Color(r, g, b)
-        
+
         artifact = Artifact()
         artifact.set_text(text)
         artifact.set_font_size(FONT_SIZE)
         artifact.set_color(color)
         artifact.set_position(position)
         artifact.set_message(message)
-        cast.add_actor("artifacts", artifact)
-    
+        cast.add_actor("artifacts", artifact)  # removes artifacts once touched
+
     # start the game
     keyboard_service = KeyboardService(CELL_SIZE)
     video_service = VideoService(CAPTION, MAX_X, MAX_Y, CELL_SIZE, FRAME_RATE)
